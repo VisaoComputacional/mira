9c8cb4198a7ec652ca5b5804ab1cb0816edb98c4
Mariana Bastos dos Santos <marianabstos@outlook.com>
2019-02-26 23:02:12 -0300

    Update PerceptualAssociativeMemory.py

--- PerceptualAssociativeMemory.py
+++ PerceptualAssociativeMemory.py
@@ -1,6 +1,6 @@
 from Workspace import Workspace
 from DeclarativeMemory import DeclarativeMemory
-#from pubsub import pub
+from pubsub import pub
 import pandas as pd
 
 class PerceptualAssociativeMemory:
@@ -13,13 +13,13 @@ class PerceptualAssociativeMemory:
         self.csm = csm
 
         # se inscreve no topico CurrentSituationalModel e publica o status do ambiente
-        #pub.subscribe(self.csm.listener, 'CurrentSituationalModel')
-        #pub.sendMessage('CurrentSituationalModel', arg1=self.getModule(), arg2=None)
+        pub.subscribe(self.csm.listener, 'CurrentSituationalModel')
+        pub.sendMessage('CurrentSituationalModel', arg1=self.getModule(), arg2=None)
 
         self.dm = DeclarativeMemory(self.user, self.csm)
         self.user_data = self.dm.getUserData()
 
-        #pub.sendMessage('CurrentSituationalModel', arg1=self.getModule(), arg2=None)
+        pub.sendMessage('CurrentSituationalModel', arg1=self.getModule(), arg2=None)
 
         self.w = Workspace(self.user_data, self.user, self.csm)
 
